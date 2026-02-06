9971792c33439b1c5ae4cda9108b8ec1e3e9ae84
Mitch1092 <mitchbart666@hotmail.com>
2026-02-06 13:56:03 -0600

    Gray image

--- prueba.py
+++ prueba.py
@@ -1,6 +1,12 @@
 import cv2 as cv
+import numpy as np
 
-img = cv.imread('./resources/Dorohedoro.jpg')
+img2 = np.ones((400,400), np.uint8)*150
+
+img = cv.imread('./resources/Dorohedoro.jpg',0)
+print(img.shape)
 cv.imshow('img', img)
+cv.imshow('img2', img2)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
